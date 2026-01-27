a547f6e1c9f369afaa7b577876cafa1fede1f54e
Kasili-T <godwinkasili146@gmail.com>
2026-01-27 11:59:41 +0300

    Update settings.py

--- koma/settings.py
+++ koma/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["koma-church-system-9.onrender.com"]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
